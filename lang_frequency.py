8f120e93ae4f8ef21d54fe7abb90164103e3670d
Pavel Piskunov <pspiskunov@gmail.com>
2018-02-24 01:13:17 +0300

    код оформлен согласно требованиям PEP, уменьшен уровень вложенности, код упрощен

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,31 +1,34 @@
 import os.path
 import sys
+import collections
+
 
 def load_data(filepath):
+    text = []
     if os.path.exists(filepath):
-        text = open(filepath, 'r').read()
-        return text
+        file = open(filepath, 'r').read()
+        for line in file.split('\n'):
+            for word in line.split(' '):
+                text.append(word)
+    return text
 
 
 def get_most_frequent_words(text):
-    frequencies_dict = {}
-    for line in text.split('\n'):
-        for word in line.split(' '):
-            if len(word) - 1 == sum(1 for any in word[:-1] if any.isalpha()):
-                word = word if word[-1].isalpha() else word[:-1]
-                if word in frequencies_dict:
-                    frequencies_dict[word] += 1
-                else:
-                    frequencies_dict[word] = 1
-    return frequencies_dict
+    frequencies = collections.Counter()
+    for word in text:
+        if len(word) - 1 == sum(1 for each in word[:-1] if each.isalpha()):
+            word = word if word[-1].isalpha() else word[:-1]
+            frequencies[word] += 1
+    return frequencies
+
 
 def pprint_for_frequencies_dict(frequencies):
-    for key, value in sorted(frequencies.items(), reverse = True, key = lambda x:x[1])[:10]:
-        print (key + ' : ' + str(value))
+    for key, value in sorted(frequencies.items(), reverse=True, key=lambda x: x[1])[:10]:
+        print(key + ' : ' + str(value))
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print ('Module need a correct file path as argument')
+        print('Module need a correct file path as argument')
     else:
         loaded_text = load_data(sys.argv[1])
         frequencies_dict = get_most_frequent_words(loaded_text)
