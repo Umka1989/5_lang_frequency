1b16beb90eb002f626293f089ca51fae01ef3138
Pavel Piskunov <pspiskunov@gmail.com>
2018-02-25 21:22:01 +0300

    исправлен вывод данных

--- lang_frequency.py
+++ lang_frequency.py
@@ -10,35 +10,31 @@ def load_data(filepath):
     return unsplited_text
 
 
-def splitter(text):
+def text_to_list(unsplited_text):
+    #return formated list of words without digits and punctuation marks
     list_of_words = []
-    for line in text.split('\n'):
+    for line in unsplited_text.upper().split('\n'):
         for word in line.split(' '):
-            list_of_words.append(word.upper())
+            if len(word) - 1 == sum(1 for each in word[:-1] if each.isalpha()):
+                #for words with punctuation marks on the end
+                word = word if word[-1].isalpha() else word[:-1]
+                list_of_words.append(word)
     return list_of_words
 
 
-def get_most_frequent_words(text):
-    frequencies_counter = collections.Counter()
-    for word in text:
-        if len(word) - 1 == sum(1 for each in word[:-1] if each.isalpha()):
-            word = word if word[-1].isalpha() else word[:-1]
-            frequencies_counter[word] += 1
-    return frequencies_counter
+def get_most_frequent_words(list_of_words):
+    frequencies_counter = collections.Counter(list_of_words)
+    ten_most_frequent_word = frequencies_counter.most_common(10)
+    print("{} most frequent word".format(str(len(ten_most_frequent_word))))
+    for word, frequency in ten_most_frequent_word:
+        print(word)
 
-
-def pprint_for_frequencies_dict(frequencies_counter):
-    """функция принимает на вход счётчик частот слов сортирует анонимной функцией,
-    которая в каждой паре "слово: частота" выбирает частоту.
-    revers заставляет сортировку работать от большего к меньшему.
-    выбирается не более 10 самых частовстречающихся слов и вывыдотся в красивом формате"""
-    for word, frequency in sorted(frequencies_counter.items(), reverse=True, key=lambda x: x[1])[:10]:
-        print(word + ' : ' + str(frequency))
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Module need a correct file path as argument')
     else:
-        loaded_text = splitter(load_data(sys.argv[1]))
-        frequencies_dict = get_most_frequent_words(loaded_text)
-        pprint_for_frequencies_dict(frequencies_dict)
+        file_content = load_data(sys.argv[1])
+        list_of_words = text_to_list(file_content)
+        get_most_frequent_words(list_of_words)
+
